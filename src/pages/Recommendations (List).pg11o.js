97030dac709cf642a9b317d1458388b385afa3ff
itay <itaibarak12345@gmail.com>
2025-06-05 10:10:46 +0300

    V1_catagory_works!

--- src/pages/Recommendations (List).pg11o.js
+++ src/pages/Recommendations (List).pg11o.js
@@ -1,49 +1,80 @@
+
+import wixData from "wix-data";
 // Velo API Reference: https://www.wix.com/velo/reference/api-overview/introduction
 //let catagories_selected = new Set();
 //let tags_selected = new Set();
 //let catagorySelectedID = "";
 let t;
 const itemColors = {defualt: "#cebdb2", "active":"#eeb591"}
-/*
+
+
+function getRowsFromCollection(colNanme) {
+    wixData.query(colNanme) // Replace with your collection name
+        .find()
+        .then((results) => {
+            if(results.items.length > 0) {
+                console.log(results.items); // Array of collection rows
+                // You can now use results.items as needed
+            } else {
+                console.log("No items found");
+            }
+        })
+        .catch((err) => {
+            console.error("Query failed:", err);
+        });
+}
+
+
+
+
 $w.onReady(function () {
+	//getRowsFromCollection('Recommendations');
+
+	
+	let catagories_selected = new Map(); //chose a map over a normal object becuase its more optimized for frequent removal of keys
+	let catagory_selected=null;
 
 	$w("#catagoriesReapeater").onItemReady(($item, itemData, index) => {
 		$item("#catagoryBox").style.backgroundColor= itemColors.defualt
 		$item("#catagoryContainer").onClick(async () => {
+			$w("#catagoriesReapeater").forItems([catagory_selected?._id],($item, itemData)=>{
+				$item("#catagoryBox").style.backgroundColor= itemColors.defualt
+				console.log(`previously chose ${itemData.title}`)
+			})
+			$item("#catagoryBox").style.backgroundColor= itemColors.active
+			catagory_selected = itemData
 			
-			if(catagorySelectedID != itemData.id){
-				
-				$w("#catagoriesReapeater").forItems([catagorySelectedID], ($item, itemData, index) =>{
-					$item("#catagoryBox").style.backgroundColor= itemColors.defualt
-					console.log(itemData.title)
-				})
-				
-				$item("#catagoryBox").style.backgroundColor= itemColors.active
-				catagorySelectedID = itemData.id
-			
-			
-    // handle delete button click
-	}})
-	})*/
 
-let catagories_selected = new Set();
-
+			$w('#RecommendationsDataset').setFilter(wixData.filter().eq('catagory',itemData._id))
+		})
+	})
+		/*
 	$w("#catagoriesReapeater").onItemReady(($item, itemData, index) => {
 		$item("#catagoryBox").style.backgroundColor= itemColors.defualt
 		$item("#catagoryContainer").onClick(async () => {
-			if(catagories_selected.has(itemData.title)){
-				catagories_selected.delete(itemData.title)
+			
+			if(catagories_selected.delete(itemData._id)){ //delete returns true if the key was deleted, false if not found
 				$item("#catagoryBox").style.backgroundColor= itemColors.defualt
 			
 			}else{
-				catagories_selected.add(itemData.title)
+				catagories_selected.set(itemData._id, itemData)
 				$item("#catagoryBox").style.backgroundColor= itemColors.active
 			
 			}
+
+			let current_filter = wixData.filter()
+		catagories_selected.forEach((v, k, map)=>{
+			current_filter = current_filter.and(wixData.filter().eq('catagory',k))
+		})
+
+			$w('#RecommendationsDataset').setFilter(
+  wixData.filter().eq('catagory',itemData._id))
 			console.log(catagories_selected)
     // handle delete button click
 		})
-	})
+
+		
+	})*/
 	// Write your Javascript code here using the Velo framework API
 
 	// Print hello world:
