9656182fbc6fb8d9df97cae5e0581ff19bd1cbd9
Itay Barak <62212254+RoybOG@users.noreply.github.com>
2025-06-17 12:17:02 +0300

    V2 - Recommendations (List) - supports catagory and tag searching

--- src/pages/Recommendations (List).pg11o.js
+++ src/pages/Recommendations (List).pg11o.js
@@ -1,103 +1,154 @@
-
 import wixData from "wix-data";
-// Velo API Reference: https://www.wix.com/velo/reference/api-overview/introduction
-//let catagories_selected = new Set();
-//let tags_selected = new Set();
-//let catagorySelectedID = "";
-let t;
-const itemColors = {defualt: "#cebdb2", "active":"#eeb591"}
 
-
-function getRowsFromCollection(colNanme) {
-    wixData.query(colNanme) // Replace with your collection name
-        .find()
-        .then((results) => {
-            if(results.items.length > 0) {
-				 console.log(results.items); // Array of collection rows
-				
-               
-                // You can now use results.items as needed
-            } else {
-                console.log("No items found");
-            }
-        })
-        .catch((err) => {
-            console.error("Query failed:", err);
-        });
-}
-
+const itemColors = {defualt: "#F6EBE4", "active":"#eeb591"}
 
 let tags_selected = new Map(); //chose a map over a normal object becuase its more optimized for frequent removal of keys
 let catagory_selected=null;
+
+
+
 
 function update_filter(){
 	let newDatasetFilter =  wixData.filter()
 	
 	if(catagory_selected){
-		newDatasetFilter = newDatasetFilter.eq('catagory',catagory_selected._id)
+		newDatasetFilter = newDatasetFilter.eq('category',catagory_selected._id)
 	}
 
-	if(tags_selected.size > 0){
-		//tags_selected.forEach((v, k, map)=>{
-	newDatasetFilter = newDatasetFilter.hasAll("tag_ids", Array.from(tags_selected.keys()));
-	/*tags_selected.forEach((v, k, map)=>{
-		newDatasetFilter = newDatasetFilter.and(wixData.filter().eq('catagory',k))
-	})*/
+	 if(tags_selected.size > 0){
+	console.log(tags_selected)
+	 newDatasetFilter = newDatasetFilter.hasAll("tagIDs", Array.from(tags_selected.keys()));
+	
 	}
 
-	$w('#RecommendationsDataset').setFilter(newDatasetFilter)
+	$w('#RecommandationsDataset').setFilter(newDatasetFilter)
 }
 
 
+function handleTabContent(openTab=false){
+	const tabContent = $w("#searchtabs").currentTab.children[0];
+	if(openTab){
+		tabContent.expand();
+	}else{
+		tabContent.collapse();
+	}
+}
+
+function toggleTabContent(){
+	const tabContent = $w("#searchtabs").currentTab.children[0];
+	if(tabContent.collapsed){
+		tabContent.expand();
+	}else{
+		tabContent.collapse();
+	}
+}
+
+
+function resetCategory(new_category_data=null){
+	$w("#catRepeater").forItems([catagory_selected?._id],($item, itemData)=>{
+					$item("#catBox").style.backgroundColor= itemColors.defualt
+	})
+	catagory_selected = new_category_data
+}
+
+
+function resetTags(){
+	$w("#tagsRepeater").forItems(Array.from(tags_selected.keys()),($item, itemData)=>{
+					$item("#tagBox").style.backgroundColor= itemColors.defualt
+	})
+	tags_selected.clear()
+}
+
+
+
 $w.onReady(function () {
-	getRowsFromCollection('Tags');
 
- 	//handling the catagories
-	
+	//-------------------- tabs setup ------------------------
 
-	$w("#catagoriesRepeater").onItemReady(($item, itemData, index) => {
-		$item("#catagoryBox").style.backgroundColor= itemColors.defualt
+	$w("#searchtabs").onChange((e)=>{
+		handleTabContent(true)
+	})
+	$w('#buttonToggle').onClick(()=>{
+		toggleTabContent()
 		
-		$item("#catagoryContainer").onClick(async () => {
+	})
+
+	handleTabContent(false) //collapsed by defualt
+
+
+
+	//------------catagory reapter setup -------------
+
+	const catButton = $w("#allCatButton")
+	$w("#catRepeater").onItemReady(($item, itemData, index) => {
+		const itemBox =  $item("#catBox")
+		if(catagory_selected?._id == itemData._id){
+			itemBox.style.backgroundColor= itemColors.active
+		}
+		$item("#catContainer").onClick(async () => {
+			catButton.enable();
 			if(catagory_selected?._id != itemData._id){
-				$w("#catagoriesRepeater").forItems([catagory_selected?._id],($item, itemData)=>{
-					$item("#catagoryBox").style.backgroundColor= itemColors.defualt
-					// console.log(`previously chose ${itemData.title}`)
-				})
-					
-				$item("#catagoryBox").style.backgroundColor= itemColors.active
-				catagory_selected = itemData
-			
+				itemBox.style.backgroundColor= itemColors.active
+				resetCategory(itemData)
+				console.log(catagory_selected)
+
 				update_filter()
 			}
 		})
-		
 	})
-		
+	
+	
+	catButton.disable()
+
+	catButton.onClick(()=>{
+		resetCategory()
+		update_filter()
+		catButton.disable()
+	})
+
+
+	//------------tags reapter setup -------------
+	const tagButton = $w("#allTagsButton")
 	$w("#tagsRepeater").onItemReady(($item, itemData, index) => {
-		$item("#tagBox").style.backgroundColor= itemColors.defualt
+		const itemBox = $item("#tagBox")
+		if(tags_selected.has(itemData._id)){
+			itemBox.style.backgroundColor= itemColors.active
+		}
 		$item("#tagContainer").onClick(async () => {
 			
+			
+			
 			if(tags_selected.delete(itemData._id)){ //delete returns true if the key was deleted, false if not found
-				$item("#tagBox").style.backgroundColor= itemColors.defualt
+				itemBox.style.backgroundColor= itemColors.defualt
 			
 			}
 			else{
 				tags_selected.set(itemData._id, itemData)
-				$item("#tagBox").style.backgroundColor= itemColors.active
+				itemBox.style.backgroundColor= itemColors.active
 			
 			}
-
-			
-		//
+			console.log(tags_selected)
 			update_filter()
-			//$w('#RecommendationsDataset').setFilter(current_filter)
-			})
-    // handle delete button click
-		
 
-		
+			if(tags_selected.size == 0){
+				tagButton.disable()
+			}else{
+				tagButton.enable()
+			}
+			
+
+		})
 	})
 	
+	
+	tagButton.disable()
+
+	tagButton.onClick(()=>{
+		resetTags()
+		update_filter()
+		tagButton.disable()
+	})
+
+
 
 });
